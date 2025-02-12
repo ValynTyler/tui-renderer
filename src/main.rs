376ebc7cf7323dc28e0753556279af79fd4c8e55
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-12 17:02:41 +0200

    feat: impl more methods on canvas

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use tui_renderer::terminal::canvas::Canvas;
 
 fn main() {
-    let canvas = Canvas::new(5, 4);
+    let canvas = Canvas::new(10, 4);
 
     println!("{}", canvas);
+    println!("{}x{}", canvas.width(), canvas.height());
 }
--- src/terminal/canvas/mod.rs
+++ src/terminal/canvas/mod.rs
@@ -2,13 +2,25 @@ use std::fmt::Display;
 
 pub struct Canvas{
     string: String,
+    _width: usize,
+    _height: usize,
 }
 
 impl Canvas {
     pub fn new(width: usize, height: usize) -> Self {
         Canvas {
-            string: ('.'.to_string().repeat(width) + "\r\n").repeat(height)
+            string: ('.'.to_string().repeat(width) + "\r\n").repeat(height - 1) + &'.'.to_string().repeat(width),
+            _width: width,
+            _height: height,
         }
+    }
+
+    pub fn width(&self) -> usize {
+        self._width
+    }
+
+    pub fn height(&self) -> usize {
+        self._height
     }
 }
 
