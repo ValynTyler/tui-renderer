c9f6dc8b39e105a68d5a618069ae4f21e3b15117
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-12 20:32:34 +0200

    feat: rendering in the center of the screen

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,14 @@ fn main() {
     square.set((2, 2), '2');
     square.set((3, 3), '3');
 
-    let canvas = Canvas::new(90, 14, '.')
+    let canvas = Canvas::new(91, 14, '.');
+
+    let x = canvas.width() / 2 - string.width() / 2;
+    let y = canvas.height() / 2 - string.height();
+    let canvas = canvas
         .render((5, 5), &square)
-        .render((86, 12), &string);
+        .render((86, 12), &string)
+        .render((x, y), &string);
 
     println!("{}", canvas);
 }
