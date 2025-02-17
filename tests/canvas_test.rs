37ecfa2debe07927b8e0677eb39b652b638cc10a
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-17 17:25:17 +0200

    chore: update canvas test formatting

--- tests/canvas_test.rs
+++ tests/canvas_test.rs
@@ -1,31 +1,6 @@
 use tui_renderer::terminal::canvas::Canvas;
 
 #[test]
-fn test_canvas_get() {
-    let canvas = Canvas::new(50, 10, '.');
-
-    for i in 0..10 {
-        for j in 0..50 {
-            assert_eq!('.', canvas.get((j, i)))
-        }
-    }
-}
-
-#[test]
-fn test_canvas_set() {
-    let mut canvas = Canvas::new(50, 10, '.');
-
-    for i in 0..10 {
-        for j in 0..50 {
-            let pos = (j, i);
-            assert_eq!('.', canvas.get(pos));
-            canvas.set(pos, '$');
-            assert_eq!('$', canvas.get(pos));
-        }
-    }
-}
-
-#[test]
 fn test_canvas_width() {
     const MAX_LEN: usize = 5;
     for i in 0..MAX_LEN {
@@ -55,6 +30,31 @@ fn test_canvas_size() {
         for j in 0..MAX_LEN {
             let canvas = Canvas::new(j, i, '.');
             assert_eq!((j, i), canvas.size());
+        }
+    }
+}
+
+#[test]
+fn test_canvas_get() {
+    let canvas = Canvas::new(50, 10, '.');
+
+    for i in 0..10 {
+        for j in 0..50 {
+            assert_eq!('.', canvas.get((j, i)))
+        }
+    }
+}
+
+#[test]
+fn test_canvas_set() {
+    let mut canvas = Canvas::new(50, 10, '.');
+
+    for i in 0..10 {
+        for j in 0..50 {
+            let pos = (j, i);
+            assert_eq!('.', canvas.get(pos));
+            canvas.set(pos, '$');
+            assert_eq!('$', canvas.get(pos));
         }
     }
 }
